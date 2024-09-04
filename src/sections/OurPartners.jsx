96b90540690aebb24e2a15d25903901fe8ad72da
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-09-04 21:41:57 +0530

    dsds

--- src/sections/OurPartners.jsx
+++ src/sections/OurPartners.jsx
@@ -4,46 +4,49 @@ function OurPartners() {
   return (
     <section>
       <div className=" container-wrapper">
-
         <div className=" py-3">
-        <h1
-          
-            className=" text-center text-3xl sm:text-5xl font-manbold font-bold py-2"
-          >
+          <h1 className=" text-center text-3xl sm:text-5xl font-manbold font-bold py-2">
             Our Team
           </h1>
         </div>
-        <div className=" flex justify-between items-center gap-4">
-          <div className=" nft p-4 border border-[#bebebe] text-center">
+        <div className=" flex flex-col sm:flex-row justify-center  items-center gap-4">
+          <div className="  nft p-4 border border-[#bebebe] text-center h-full">
             <img src="images/sara.png" alt="" />
 
             <p>Sara</p>
 
-            <p>Business Development Manage</p>
+            <p className=" text-xs">Business Development Manager</p>
           </div>
 
-          <div className=" nft p-4 border border-[#bebebe] text-center ">
+          <div className="  nft p-4 border border-[#bebebe] text-center h-full ">
             <img src="images/david.png" alt="" />
 
             <p>Ashish</p>
 
-            <p>Marketing Head</p>
+            <p>Marketing manager</p>
           </div>
 
-          <div className=" nft p-4 border border-[#bebebe] text-center">
+          <div className="  nft p-4 border border-[#bebebe] text-center h-full">
             <img src="images/gur.png" alt="" />
             <p>Gurpreet</p>
 
-<p> Co-Founder</p>
-         
+            <p> Co-Founder</p>
           </div>
 
-          <div className=" nft p-4 border border-[#bebebe] text-center">
+          <div className="  nft p-4 border border-[#bebebe] text-center h-full">
             <img src="images/noor2.png" alt="" />
 
-            <p>Ritesh</p>
+            <p>Ronnie</p>
 
-<p>Founder</p>
+            <p>Founder</p>
+          </div>
+
+          <div className="  nft p-4 border border-[#bebebe] text-center h-full">
+            <img src="images/david2.png" alt="" />
+
+            <p>David </p>
+
+            <p> Partnership Manager</p>
           </div>
         </div>
       </div>
